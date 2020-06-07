e744cbd30c9b2fb270d4f1e02561ae90b4918a89
Morne <morneventer.mv@gmail.com>
2020-06-08 01:23:54 +0200

    f

--- Actors/Cofalt/Cofalt.gd
+++ Actors/Cofalt/Cofalt.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
-
 func _ready():
 	$AnimationPlayer.current_animation = 'idle'
-
-
-
 
 func _on_TriggerArea_area_entered(area):
 	if area.name == "PlayerHitLeft":
 		$AnimationPlayer.current_animation = "hit_left"
+		hit()
 	elif area.name == "PlayerHitRight":
 		$AnimationPlayer.current_animation = "hit_right"
+		hit()
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "hit_left" or anim_name == "hit_right":
 		$AnimationPlayer.current_animation = "idle"
+
+func hit():
+	$FloatingTextManager.showValue('+1', false)
---
+++ UI/FloatingTextManager.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var FCT = preload("./FCT.tscn")
+
+export var travel = Vector2(0,-80)
+export var duration = 1.5
+export var spread = PI/2
+
+func showValue(value, crit = false):
+	var fct = FCT.instance()
+	add_child(fct)
+	fct.showValue(str(value), travel, duration, spread, crit)
