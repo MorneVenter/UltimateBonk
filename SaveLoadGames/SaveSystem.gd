3209f26134c3dc96aae5593a13c654c93a1c9e0c
MorneVenter <morneventer.mv@gmail.com>
2020-06-14 00:49:47 +0200

    f

--- SaveLoadGames/SaveSystem.gd
+++ SaveLoadGames/SaveSystem.gd
@@ -3,7 +3,7 @@ var data : Dictionary = {}
 var SAVEFOLDER = "user://savedata"
 var SAVE_NAME_TEMPLATE = "save_%03d.save"
 var id: int = 1
-
+var key = 'WhYaReyOuTrYing123To%acCesSyOuRSaVEgamE?plSsToP'
 
 func _ready():
 	pass
@@ -14,7 +14,7 @@ func _loadData():
 	if not file.file_exists(save_path):
 		_init_save_file(save_path)
 	
-	file.open(save_path, File.READ)
+	file.open_encrypted_with_pass(save_path, File.READ, key)
 	data = file.get_var()
 	file.close()
 
@@ -23,14 +23,14 @@ func _init_save_file(savepath: String):
 	var directory = Directory.new()
 	directory.make_dir(SAVEFOLDER)
 	var save_game = File.new()
-	save_game.open(savepath, File.WRITE)
+	save_game.open_encrypted_with_pass(savepath, File.WRITE, key)
 	save_game.store_var(data)
 	save_game.close()
 	
 func _update_save_file():
 	var save_path = SAVEFOLDER.plus_file(SAVE_NAME_TEMPLATE % id)
 	var save_game = File.new()
-	save_game.open(save_path, File.WRITE)
+	save_game.open_encrypted_with_pass(save_path, File.WRITE, key)
 	save_game.store_var(data)
 	save_game.close()
 
